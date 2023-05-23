9d054b08a74f7b59ab38cd5cecc09122a8596dfc
Jurij Jusel <jjusel@gmail.com>
2023-05-23 22:12:41 +0300

    app.py changes

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from bs4 import BeautifulSoup
-import requests
+# from bs4 import BeautifulSoup
+# import requests
 from utils.file import create_json
-
+from urllib.request import Request, urlopen
 
 url = 'https://www.glassdoor.com/member/home/index.htm'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
 
+
+
+# req = Request(url=url, headers=headers)
+# webpage = urlopen(req).read()
+# print(webpage)
 
 # def download_meta():
-meta_data=[]
-req = requests.get(url, headers=headers)
+# meta_data=[]
+# req = requests.get(url, headers=headers)
 # soup = BeautifulSoup(req.content, features="lxml")
-soup = BeautifulSoup(req.content, 'html.parser')
+# soup = BeautifulSoup(req.content, 'html.parser')
 # meta_tags = soup.find_all('meta')
     # print(meta_tags)
     # meta_data.append(meta_tags)
 # for tag in meta_tags: 
 #     meta_data.append(tag.attrs)
 #     print(meta_data) 
-print(req)
+# print(req)
 # print(meta_tags)
 
 # print(create_json(meta_data, 'meta.json'))
