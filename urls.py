2f9dcb5ebaaf795df3325f30abef6a2e3596fd43
Jurij Jusel <jjusel@gmail.com>
2023-05-29 21:48:27 +0300

    urls.py changes

--- urls.py
+++ urls.py
@@ -1,6 +1,13 @@
-urls = ['https://pricer.lt/tyrimai/kuras/orlen-lietuva',
-        'https://www.circlek.lt/privatiems/degalu-kainos',
-        'https://www.abromika.lt/#',
-        'https://www.saurida.lt/degalines/#kuro-kainos',
-        
+urls = [
+    {'name': 'Orlen Lietuva', 'url': 'https://pricer.lt/tyrimai/kuras/orlen-lietuva'},
+    {'name': 'Circle', 'url': 'https://www.circlek.lt/privatiems/degalu-kainos'},
+    {'name': 'Viada', 'url': 'https://gasprices.dna.lv/lt/brand/viada'},
+    {'name': 'Neste', 'url': 'https://www.kuro-kainos.lt/degalu-kainos/neste'},
+    {'name': 'Baltic Petroleum',
+        'url': 'https://gasprices.dna.lv/lt/brand/baltic-petroleum'},
+    {'name': 'Alausa', 'url': 'https://gasprices.dna.lv/lt/station/52647/alausa'},
+    {'name': 'Jozita', 'url': 'http://www.degalukainos.lt'},
+    {'name': 'Emsi', 'url': 'https://www.kuro-kainos.lt/degalu-kainos/emsi'},
+    {'name': 'Takuras', 'url': 'http://www.degalukainos.lt/map.php?id=765'},
+    {'name': 'Abromika', 'url': 'https://www.abromika.lt/#'}
 ]
