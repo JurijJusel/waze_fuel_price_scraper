367e141492cd5a1160b3b1b361c119044a0fc3cc
Jurij Jusel <jjusel@gmail.com>
2023-06-09 00:54:47 +0300

    create station.py

---
+++ station.py
@@ -0,0 +1,7 @@
+
+class Station:
+    def __init__(self, name):
+        self.name = name
+        self.url = self.get_url_by_name()
+        self.headers = headers
+        self.json_file = 'fuel1.json'
