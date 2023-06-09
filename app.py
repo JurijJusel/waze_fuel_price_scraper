06cb4c84965d84541ff40b915ac7a666841ba823
Jurij Jusel <jjusel@gmail.com>
2023-06-09 20:00:23 +0300

    station circle slass changes

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
-import time
 from utils.file import create_json
 from constants import headers, fuel_types_circle
 from urls import urls
-
+from station_circle import Station_circle
 
 class FuelCrawler:
     def __init__(self, name):
         self.name = name
         self.url = self.get_url_by_name()
+        self.station_circle = Station_circle()
         self.headers = headers
         self.json_file = 'fuel1.json'
 
@@ -29,44 +29,22 @@ class FuelCrawler:
             raise Exception("Failed to download response")
 
 
-    def circlek_prices(self, soup):
-        title = soup.title.string[-8::]
-        cards = soup.find_all('div', {'class': 'atom-card'})
-        updated_fuel_info = cards[0].text[-26:-5]
-        price_miles_95 = cards[0].text[43:48]
-        price_miles_plus_95 = cards[1].text[43:48]
-        price_miles_D = cards[3].text[43:48]
-        price_miles_D_plus = cards[4].text[43:48]
-
-        time_now = time.strftime("%Y-%m-%d %H:%M:%S")
-        
-        self.circlek_data = {
-            'company': title,
-            'scrap_time': time_now,
-            'updated_fuel_info': updated_fuel_info, 
-            'miles_95': price_miles_95, 
-            'miles_plus_95': price_miles_plus_95,
-            'miles_D': price_miles_D,
-            'miles_D_plus': price_miles_D_plus,
-            }      
-        return self.circlek_data
-
-
     def get_prices(self):
         try:
             soup_response = self.download_response()
-            return self.circlek_prices(soup_response)
+            self.circle_data = self.station_circle.circlek_prices(soup_response)
+            return self.circle_data
         except Exception as e:
             print("Error exception info:", str(e))
         
-       
+      
     def print_data(self):
         self.get_prices()
-        create_json(self.circlek_data, self.json_file )
-        updated_fuel_info = self.circlek_data['updated_fuel_info']
-        company = self.circlek_data['company']
+        create_json(self.circle_data, self.json_file )
+        updated_fuel_info = self.circle_data['updated_fuel_info']
+        company = self.circle_data['company']
         for fuel_type in fuel_types_circle:
-            fuel_value = self.circlek_data[fuel_type]
+            fuel_value = self.circle_data[fuel_type]
             print(f"{updated_fuel_info}:  {company}  {fuel_type} - {fuel_value}")
       
     
