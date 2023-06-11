c93f7a0fcf418f82e34acdc7f1c4c1221dd1add1
Jurij Jusel <jjusel@gmail.com>
2023-06-11 20:58:42 +0300

    add def __str__()

--- station.py
+++ station.py
@@ -1,7 +1,7 @@
 
 class Station:
     def __init__(self, company, adress, fuel_updated_date, name_D, price_D, name_A95, price_A95):
-        self.name = company,
+        self.company = company,
         self.adress = adress,
         self.fuel_updated_date = fuel_updated_date,
         self.name_D = name_D,
@@ -12,15 +12,14 @@ class Station:
   
         self.data = {
             'company': self.company,
-            ' adress': self.adress,
+            'adress': self.adress,
             'fuel_updated_date': self.fuel_updated_date, 
             self.name_D: self.price_D,
             self.name_A95: self.price_A95       
         }
         
-    def print_data(self):
-        print(f"{self.company}, {self.adress}, {self.fuel_updated_date}, {self.name_D}: {self.price_D}, {self.name_A95}: {self.price_A95}") 
+    def __str__(self):
+        return f"company: {self.company}, adress: {self.adress}, fuel_updated_date: {self.fuel_updated_date}, {self.name_D}: {self.price_D}, {self.name_A95}: {self.price_A95}" 
         
    
-  
   
