971a6135c5c771c990a8bfe9ff545285af1a53b9
Jurij Jusel <jjusel@gmail.com>
2023-06-17 13:20:42 +0300

    create logs.py

---
+++ logs.py
@@ -0,0 +1,20 @@
+class Log:
+    def __init__(self, ):
+        pass
+    
+    
+    
+    
+    
+    
+    def data_to_txt(self):
+        pass
+
+   
+   
+   
+   
+   
+   
+    def __str__(self):
+        pass
