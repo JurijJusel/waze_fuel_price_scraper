25740e6ba1b577574627d1598bffe2c1f1515530
Jurij Jusel <jjusel@gmail.com>
2023-06-30 22:38:55 +0300

    optimize def status_log

--- logs.py
+++ logs.py
@@ -16,35 +16,63 @@ class Logger:
         self.resp_err = obj.resp_err
         
     
-    def status_log(self):
-        if self.status == 200: 
-            log_status = f"{self.timestamp}, status code: {self.status}, {self.name}"
-            self.write_logs(log_status, "logs.txt")
-            if  self.fuel_updated_date == None:
-                log_fuel_updated_date = f"{self.timestamp}, status code: {self.status}, {self.name}, error get fuel_updated_date"
-                self.write_logs(log_fuel_updated_date, "logs.txt")
-            if self.company == None:
-                log_company = f"{self.timestamp}, status code: {self.status}, {self.name}, error get company"
-                self.write_logs(log_company, "logs.txt")
-            if self.adress == None:
-                log_adress = f"{self.timestamp}, status code: {self.status}, {self.name}, error get adress"
-                self.write_logs(log_adress, "logs.txt")
-            if self.name_D == None:
-                log_name_D = f"{self.timestamp}, status code: {self.status}, {self.name}, error get name_D"
-                self.write_logs(log_name_D, "logs.txt")    
-            if self.price_D == None:
-                log_price_D = f"{self.timestamp}, status code: {self.status}, {self.name}, error get price_D"
-                self.write_logs(log_price_D, "logs.txt") 
-            if self.name_A95 == None:
-                log_name_A95 = f"{self.timestamp}, status code: {self.status}, {self.name}, error get name_A95"
-                self.write_logs(log_name_A95, "logs.txt")    
-            if self.price_A95 == None:
-                log_price_A95 = f"{self.timestamp}, status code: {self.status}, {self.name}, error get price_A95"
-                self.write_logs(log_price_A95, "logs.txt")      
-        else:
-            log_status =  f"{self.timestamp}, {self.name}, error: {self.resp_err}"   
-            self.write_logs(log_status, "logs.txt") 
+    # def status_log(self):
+    #     if self.status == 200: 
+    #         log_status = f"{self.timestamp}, status code: {self.status}, {self.name}"
+    #         self.write_logs(log_status, "logs.txt")
+    #         if  self.fuel_updated_date == None:
+    #             log_fuel_updated_date = f"{self.timestamp}, status code: {self.status}, {self.name}, error get fuel_updated_date"
+    #             self.write_logs(log_fuel_updated_date, "logs.txt")
+    #         if self.company == None:
+    #             log_company = f"{self.timestamp}, status code: {self.status}, {self.name}, error get company"
+    #             self.write_logs(log_company, "logs.txt")
+    #         if self.adress == None:
+    #             log_adress = f"{self.timestamp}, status code: {self.status}, {self.name}, error get adress"
+    #             self.write_logs(log_adress, "logs.txt")
+    #         if self.name_D == None:
+    #             log_name_D = f"{self.timestamp}, status code: {self.status}, {self.name}, error get name_D"
+    #             self.write_logs(log_name_D, "logs.txt")    
+    #         if self.price_D == None:
+    #             log_price_D = f"{self.timestamp}, status code: {self.status}, {self.name}, error get price_D"
+    #             self.write_logs(log_price_D, "logs.txt") 
+    #         if self.name_A95 == None:
+    #             log_name_A95 = f"{self.timestamp}, status code: {self.status}, {self.name}, error get name_A95"
+    #             self.write_logs(log_name_A95, "logs.txt")    
+    #         if self.price_A95 == None:
+    #             log_price_A95 = f"{self.timestamp}, status code: {self.status}, {self.name}, error get price_A95"
+    #             self.write_logs(log_price_A95, "logs.txt")      
+    #     else:
+    #         log_status =  f"{self.timestamp}, {self.name}, error: {self.resp_err}"   
+    #         self.write_logs(log_status, "logs.txt") 
         
+    def status_log(self):
+        log_messages = []
+
+        if self.status == 200:
+            log_status = f"{self.timestamp}, status code: {self.status}, {self.name}"
+            log_messages.append(log_status)
+
+            attribute_checks = {
+                "fuel_updated_date": "error get fuel_updated_date",
+                "company": "error get company",
+                "adress": "error get adress",
+                "name_D": "error get name_D",
+                "price_D": "error get price_D",
+                "name_A95": "error get name_A95",
+                "price_A95": "error get price_A95"
+            }
+
+            for attribute, error_message in attribute_checks.items():
+                if getattr(self, attribute) is None:
+                    log_message = f"{self.timestamp}, status code: {self.status}, {self.name}, {error_message}"
+                    log_messages.append(log_message)
+        else:
+            log_status = f"{self.timestamp}, {self.name}, error: {self.resp_err}"
+            log_messages.append(log_status)
+
+        log_text = "\n".join(log_messages)
+        self.write_logs(log_text, "logs.txt")
+       
         
     def data_value_logs(self):
         return f"{self.timestamp}, {self.name}, {self.status}, {self.url}, {self.company}, {self.adress}"  #, {self.name_D} {self.price_D}, {self.adress}"
