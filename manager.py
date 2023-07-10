b6dd6c08f24013ec30ee0449e8f30d0cbf88d198
Jurij Jusel <jjusel@gmail.com>
2023-07-10 20:29:08 +0300

    create manager.py

---
+++ manager.py
@@ -0,0 +1,26 @@
+# from app import FuelCrawler
+from run_scripts import run_scripts
+
+# run = run_scripts()
+# crawler = FuelCrawler(name="Circle")
+# print(crawler.req_status)
+# name = crawler.get_url_by_company_name()
+# print(name)
+
+class Manager:
+    def __init__(self) -> None:
+        pass
+    
+    def activate_scripts():
+        # print(crawler.req_status)
+        run_scripts()
+    
+        # else:
+        #     print("dsfdvsffv")
+    
+    def __str__(self):
+        pass
+        
+
+# manager = Manager
+# x = manager
