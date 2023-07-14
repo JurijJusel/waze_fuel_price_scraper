79c3b11f3716840140feff8452b93e4f19757c4a
Jurij Jusel <jjusel@gmail.com>
2023-07-15 00:33:31 +0300

    update urls

--- urls.py
+++ urls.py
@@ -1,13 +1,13 @@
 fuel_station_urls = [
     {'name': 'Orlen Lietuva', 'url': 'https://pricer.lt/tyrimai/kuras/orlen-lietuva'},
-    {'name': 'Circle', 'url': "https://gas.didnt.work/?country=lt&brand=Circle+K&city=Vilnius"},
+    {'name': 'Circle', 'url': 'https://gas.didnt.work/?country=lt&brand=Circle+K&city=Vilnius'},
     {'name': 'Viada', 'url': 'https://gasprices.dna.lv/lt/brand/viada'},
     {'name': 'Neste', 'url': 'https://www.kuro-kainos.lt/degalu-kainos/neste'},
     {'name': 'Baltic Petroleum', 'url': 'https://gasprices.dna.lv/lt/brand/baltic-petroleum'},
     {'name': 'Alausa', 'url': 'https://gasprices.dna.lv/lt/station/52647/alausa'},
     {'name': 'Jozita', 'url': 'http://www.degalukainos.lt'},
     {'name': 'Emsi', 'url': 'https://www.kuro-kainos.lt/degalu-kainos/emsi'},
-    {'name': 'Takuras', 'url': "https://gas.didnt.work/?country=lt&brand=Takuras&city=Vilnius"},
-    {'name': "Kvistija", "url": "https://gas.didnt.work/?country=lt&brand=Kvistija&city=Vilnius"},
-    {'name': 'Abromika', 'url': "https://www.abromika.lt/#"}
+    {'name': 'Takuras', 'url': 'https://gas.didnt.work/?country=lt&brand=Takuras&city=Vilnius'},
+    {'name': "Kvistija", "url": 'https://gas.didnt.work/?country=lt&brand=Kvistija&city=Vilnius'},
+    {'name': 'Abromika', 'url': 'https://www.abromika.lt/#'}
 ]
