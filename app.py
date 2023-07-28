09a0c347115d30b28745ed6b046b2e4f3b2edfaa
Jurij Jusel <jjusel@gmail.com>
2023-07-28 21:17:04 +0300

    update app

--- app.py
+++ app.py
@@ -19,34 +19,20 @@ class FuelCrawler:
         selected_url = next(item['url'] for item in fuel_station_urls if item['name'] == self.name)
         return selected_url
     
-    def run():
+    def run(self):
         run_s = run_scripts()  
         return run_s
                        
 if __name__ == '__main__':
     crawler = FuelCrawler(name='Circle')
     manager = Manager(status=True)
-
-    # manager = Manager(status=True).activate_scripts() #TODO taip geriau nedaryti jei as manager noriu veliau dar panaudoti,o tu ji ensi issaugojes butent kaip obj o i return ikisai runscript,tai manger savyje saugos ne manger obj o run_script return result
+    if manager.is_activate():
+        crawler.run()
+       
+    # manager = Manager(status=True).activate_scripts() #TODO taip
+    # geriau nedaryti jei as manager noriu veliau dar panaudoti,o 
+    # tu ji ensi issaugojes butent kaip obj o i return ikisai \
+        # runscript,tai manger savyje saugos ne manger obj o 
+        # run_script return result
     # manager.activate_scripts()
   
-    
-    
-     
-# else:
-#     # cr = FuelCrawler
-#     print("else from app")
-    # cr.run
-# else:
-#     # crawler = FuelCrawler(name='Circle')
-#     run_script = run_scripts()
-#     print("else")
-    
-    # crawler = FuelCrawler(name='Circle')
-    # run_script = run_scripts()
-    
-
-    # crawler = FuelCrawler(name="Circle")
-    
-    # manager.activate_scripts()
- 
