70fab7d2b713934095a3269f55f82533dab9a369
Jurij Jusel <jjusel@gmail.com>
2023-07-28 21:17:15 +0300

    update logs

--- logs.py
+++ logs.py
@@ -1,24 +1,34 @@
 import datetime
 from pathlib import Path
 
-class Crawl_logs:
-    def __init__(self, name: str, message: str, txt_file="logs.txt"):
-        self.name = name
-        self.message = message
-        self.txt_file = txt_file
+class Script_log:
+    def __init__(self):
+        self.txt_file = "logs.txt"
+        self.time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
-    # def log_entry(self):
-    #     return f"{self.time_stamp}, {self.name}, {self.message}"
-    
-    def write_log(name, message ):
-        txt_file="logs.txt"
-        time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        path = Path(f"data/{txt_file}")
-        log_status = f"{time_stamp}, {name}, {message}"
+    def write_log(self, name, message):
+        path = Path(f"data/{self.txt_file}")
+        log_status = f"{self.time_stamp}, {name}, {message}"
+        
         with open(path, 'a') as file:
             file.write(log_status + "\n")
-            print(f"Logs successfully written to <{txt_file}>.") 
+            print(f"Logs {name}, successfully written to <{self.txt_file}>.") 
             
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
          
   
 # def write_logs(name, message, txt_file="logs.txt"):
