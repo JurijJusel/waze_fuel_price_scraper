9099d32fe04392dba811e61cf4bcae3679f48430
Jurij Jusel <jjusel@gmail.com>
2023-08-02 21:18:44 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from run_scripts import run_scripts
 from manager import Manager
+from logs import Script_log
+from run_scripts import Scripts
+
 
 class FuelCrawler:
-    def __init__(self):#TODO station ir fuel-crawler ju init identiski,reiktu vienu  atsikratyti
-        self.fuel_updated_date = None
-        self.company = None
-        self.adress = None
-        self.name_D = None
-        self.price_D = None
-        self.name_A95 = None
-        self.price_A95 = None
-        self.req_status = None
-        self.url = None
-    
-    def run(self):
-        run_s = run_scripts()  
-        return run_s
-                       
+    def __init__(self):
+        pass
+       
+    def run(self, script_obj):
+        script_obj.run_scripts()
+        script_obj.print_scripts()
+                            
 if __name__ == '__main__':
     crawler = FuelCrawler()
+    script_log = Script_log()
+    scripts = Scripts()
+    
     manager = Manager(status=True)
     if manager.is_activate():
-        crawler.run()
-       
+        crawler.run(scripts)
+    
+    script_log.print_logs_obj_count()
+  
+    
