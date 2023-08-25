6d74ad1c97f17c1184874312ae3c35ad7036ffc3
Jurij Jusel <jjusel@gmail.com>
2023-08-26 00:37:43 +0300

    changes output print for info

--- connect_to_db.py
+++ connect_to_db.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path='.env')
 
+
 def db_connection():
     db_params = {
         'host': os.getenv('DB_HOST'),
@@ -16,6 +17,16 @@ def db_connection():
         connect = psycopg2.connect(**db_params)
         print(f"Connection to the '{db_params['database']}' database is successful.")
         return connect
-    except (Exception, psycopg2.DatabaseError) as err:
-        print(f"Error connecting to '{db_params['database']}' database:", err)
-
+    except psycopg2.OperationalError as err:
+        error_message = str(err)
+        if "not translate host name" in error_message:
+            print("Failed to connect: Invalid host name")
+        if "does not exist" in error_message:
+            print("Wrong database name !!!!!!!!!!!!!!!!!!!")
+        elif "password authentication failed" in  error_message:
+            print("Failed to connect: wrong user name or password")
+        else:
+            print(f"Failed connecting to the database: {error_message}")
+    except Exception as err:
+        print(f"An error connecting to the database:", err)
+    
