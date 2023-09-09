b16fad4808f5bccf9b2abe584357d48ecd410cd9
Jurij Jusel <jjusel@gmail.com>
2023-09-10 01:20:25 +0300

    update station

--- station.py
+++ station.py
@@ -1,4 +1,3 @@
-
 class Station:
     def __init__(self, company, address, fuel_updated_date, name_D, price_D, name_A95, price_A95):
         self.company = company
@@ -20,6 +19,3 @@ class Station:
 
     def __str__(self):
         return f"company: {self.company}, address: {self.address}, fuel_updated_date: {self.fuel_updated_date}, {self.name_D}: {self.price_D}, {self.name_A95}: {self.price_A95}" 
-        
-   
-  
