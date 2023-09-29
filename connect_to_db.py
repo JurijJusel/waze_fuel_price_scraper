95933930c515f206933acd800382b6e591f9e551
Jurij Jusel <jjusel@gmail.com>
2023-09-29 21:00:05 +0300

    add connect_timeout=10

--- connect_to_db.py
+++ connect_to_db.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path='.env') 
+
 db_params = {
     'host': os.getenv('DATABASE_HOST'),
     'port': int(os.getenv('DATABASE_PORT')),
@@ -13,7 +14,7 @@ db_params = {
 
 def db_connection(db_params):
     try:
-        connect = psycopg2.connect(**db_params)
+        connect = psycopg2.connect(connect_timeout=10, **db_params)
         print(f"Connection to the '{db_params['database']}' database is successful.")
         return connect
     except psycopg2.OperationalError as err:
