7e42a138333f3d24d0276581f5071f785b6facde
Jurij Jusel <jjusel@gmail.com>
2023-10-08 12:16:55 +0300

    import scripts dir from constants

--- run_scripts.py
+++ run_scripts.py
@@ -1,26 +1,24 @@
 import os
 from constants import script_run_files
 import requests
-from constants import url
+from constants import url, scripts_dir
 from script_logs import Script_log
 
-
+script_log = Script_log()
 class Scripts:
     def __init__(self) -> None:
-        self.script_dir = "scripts/"
         self.executed_files = []
         self.all_script_files = []
-        self.script_log = Script_log()
 
     def run_scripts(self):
         self.count = 0
         self.req = requests.get(url)
         if self.req.status_code == 200:          
-            for script_file in os.listdir(self.script_dir):
+            for script_file in os.listdir(scripts_dir):
                 if script_file.endswith('.py'):
                     self.all_script_files.append(script_file)  
                 if script_file[:-3] in script_run_files: 
-                    script_path = os.path.join(self.script_dir, script_file) #path = Path(f"data/{file}")
+                    script_path = os.path.join(scripts_dir, script_file) #path = Path(f"data/{file}")
                     with open(script_path, 'r') as file:
                         script_code = file.read()
                         exec(script_code, globals())  # Include globals() as second argument!
@@ -28,7 +26,7 @@ class Scripts:
                         self.executed_files.append(script_file)   
             self.print_scripts_ind()
         else:
-            self.script_log.write_log(url, f"status code: {self.req.status_code}, ConnectionError")
+            script_log.write_log(url, f"status code: {self.req.status_code}, ConnectionError")
             return None
        
     def print_scripts_ind(self):     
